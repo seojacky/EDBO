0ee7aa1691610f78918612aed006ed1f55997d9d
«aniuta17935» <ann.adamchuk2002@gmail.com>
2021-12-29 03:09:00 +0200

    institution address concat  done

--- server/src/services/institutionsService.js
+++ server/src/services/institutionsService.js
@@ -1,11 +1,12 @@
 const createConnection = require('../db');
+const { SqlError } = require('../utils/errors');
 
 const getInstitutionsByRegion = async(region, global_type) => {
     try {
         const client = createConnection();
-        const result = await client.query(`SELECT * FROM public.educational_institutions WHERE region = '${region}' AND global_type = '${global_type}';`);
+        const result = await client.query(`SELECT code_edbo, long_name, short_name, code_identification, form, concat(address, ', ', city, ', ', region) as long_adress, type, phone, email, site, year_foundation, postal_code FROM public.educational_institutions WHERE region = '${region}' AND global_type = '${global_type}';`);
         client.end();
-        return result.rows[0];
+        return result.rows;
     } catch (err) {
         throw new SqlError(err.message)
     }
@@ -14,7 +15,7 @@ const getInstitutionsByRegion = async(region, global_type) => {
 const getInstitutionsByRegionAndName = async(region, global_type, name) => {
     try {
         const client = createConnection();
-        const result = await client.query(`SELECT * FROM public.educational_institutions WHERE region = '${region}' AND global_type = '${global_type}' AND long_name LIKE '%${name}%';`);
+        const result = await client.query(`SELECT code_edbo, long_name, short_name, code_identification, form, concat(address, ', ', city, ', ', region) as long_adress, type, phone, email, site, year_foundation, postal_code FROM public.educational_institutions WHERE region = '${region}' AND global_type = '${global_type}' AND long_name LIKE '%${name}%';`);
         client.end();
         return result.rows[0];
     } catch (err) {
